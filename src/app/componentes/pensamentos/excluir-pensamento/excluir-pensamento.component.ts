34be4fd5f163745f83697975b98702772427d656
pedromesmer <pedro_mesmer@hotmail.com>
2022-11-13 16:04:42 -0300

    first commit

---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,48 @@
+import { PensamentoService } from './../pensamento.service';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css']
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento= {
+    autoria: '',
+    conteudo: '',
+    modelo: '',
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento;
+    })
+  }
+
+  excluirPensamento() {
+    if (this.pensamento.id) {
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
+
+
+
+}
